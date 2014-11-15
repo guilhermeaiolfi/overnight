c127dc9a7abbc5013ef1bebccc4fbab9bf18da60
Guilherme R. Aiolfi <gradinf@gmail.com>
2014-11-15 14:09:12 -0200

    More capable Router.

--- src/Router.php
+++ src/Router.php
@@ -9,7 +9,57 @@ class Router extends \Aura\Router\Router {
   }
   public function getBaseHref() {
     $basePath = str_replace('\\', '/', $this->application->getConfig("paths.base_url_subdir"));
-    return $_SERVER["REQUEST_SCHEME"] . "://" . $_SERVER["HTTP_HOST"] . $basePath . "/";
+    $urlHost = preg_replace('/\]\:' . preg_quote($_SERVER['SERVER_PORT'], '/') . '$/', '', $_SERVER['SERVER_NAME']) . ($this->isPortNecessary($_SERVER["REQUEST_SCHEME"], $_SERVER["SERVER_PORT"]) ? ':' . $_SERVER["SERVER_PORT"] : '');
+    if(substr($basePath, -1, 1) != '/') {
+      $basePath .= '/';
+    }
+    return $_SERVER["REQUEST_SCHEME"] . "://" . $urlHost . $basePath;
+  }
+
+  // TODO: temporary hack, it should go into the request class after things get more stable
+  private function isPortNecessary($schema, $port) {
+    return ($schema === "https" && $port != 443) || ($schema === 'http' && $port != 80);
+  }
+  // TODO: temporary hack, it should go into the request class after things get more stable
+  private function getRequestUri() {
+    if(isset($_SERVER['UNENCODED_URL']) && isset($_SERVER['SERVER_SOFTWARE']) && strpos($_SERVER['SERVER_SOFTWARE'], 'Microsoft-IIS') !== false) {
+      // Microsoft IIS 7 with URL Rewrite Module
+      return $_SERVER['UNENCODED_URL'];
+    } elseif(isset($_SERVER['HTTP_X_REWRITE_URL']) && isset($_SERVER['SERVER_SOFTWARE']) && strpos($_SERVER['SERVER_SOFTWARE'], 'Microsoft-IIS') !== false) {
+      // Microsoft IIS with ISAPI_Rewrite
+      return $_SERVER['HTTP_X_REWRITE_URL'];
+    } elseif(!isset($_SERVER['REQUEST_URI']) && isset($_SERVER['SERVER_SOFTWARE']) && strpos($_SERVER['SERVER_SOFTWARE'], 'Microsoft-IIS') !== false) {
+      return $_SERVER['ORIG_PATH_INFO'] . (isset($_SERVER['QUERY_STRING']) && $_SERVER['QUERY_STRING'] != '' ? '?' . $_SERVER['QUERY_STRING'] : '');
+    } elseif(isset($_SERVER['REQUEST_URI'])) {
+      return $_SERVER['REQUEST_URI'];
+    }
+  }
+  public function addRoute($name, $route) {
+    $r = $this->add($name, $route["pattern"]);
+    foreach ($route as $key => $values) {
+      if (in_array($key, array('tokens', 'values', 'server', 'accept')))
+      {
+        $method = 'add' . ucfirst($key);
+        $r->$method($values);
+        $route[$key] = null;
+      }
+      else if (in_array($key, array('secure', 'wildcard', 'routable', 'isMatchCallable', 'generateCallable')))
+      {
+        $method = 'set' . ucfirst($key);
+        $r->$method($values);
+        $route[$key] = null;
+      }
+    }
+    $r->addValues($route);
+  }
+  public function addRoutes($routes) {
+    if (!$routes) {
+      return;
+    }
+    foreach ($routes as $name => $route) {
+      $this->addRoute($name, $route);
+    }
+    return $this;
   }
 }
 ?>
