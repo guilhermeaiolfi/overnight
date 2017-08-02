76da3f6a642f98ee55c589e4cfe1a6c9dc4d5350
Guilherme Aiolfi <gradinf@gmail.com>
2017-08-02 17:14:46 -0300

    Middleware to define the output type

---
+++ src/Middleware/OutputTypeMiddleware.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace ON\Middleware;
+use Interop\Http\ServerMiddleware\MiddlewareInterface as ServerMiddlewareInterface;
+use Interop\Http\ServerMiddleware\DelegateInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseInterface;
+use Zend\Expressive\Router\RouteResult;
+use Zend\Expressive\Router\RouterInterface;
+
+class OutputTypeMiddleware implements ServerMiddlewareInterface
+{
+    protected $container = null;
+
+    public function __construct(ContainerInterface $container) {
+        $this->container = $container;
+    }
+
+    public function process (ServerRequestInterface $request, DelegateInterface $delegate)
+    {
+        $accept = $request->getHeader('Accept')[0];
+        if (!$accept || ! preg_match('#^application/([^+\s]+\+)?json#', $accept)) {
+            $request = $request->withAttribute("OUTPUT_TYPE", "html");
+        } else {
+          $request = $request->withAttribute("OUTPUT_TYPE", "json");
+        }
+
+        return $delegate->process($request);
+    }
+}
+?>
+
