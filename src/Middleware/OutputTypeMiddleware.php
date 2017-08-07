03d6bd6bd110a2efbb70eab431f8155666f1607a
Guilherme Aiolfi <gradinf@gmail.com>
2017-08-07 15:43:31 -0300

    Fix extra line that was causing problems to the PhpDebugBar

--- src/Middleware/OutputTypeMiddleware.php
+++ src/Middleware/OutputTypeMiddleware.php
@@ -1,6 +1,6 @@
 <?php
-
 namespace ON\Middleware;
+
 use Interop\Http\ServerMiddleware\MiddlewareInterface as ServerMiddlewareInterface;
 use Interop\Http\ServerMiddleware\DelegateInterface;
 use Psr\Http\Message\ServerRequestInterface;
@@ -11,23 +11,14 @@ use Zend\Expressive\Router\RouterInterface;
 
 class OutputTypeMiddleware implements ServerMiddlewareInterface
 {
-    protected $container = null;
-
-    public function __construct(ContainerInterface $container) {
-        $this->container = $container;
-    }
-
     public function process (ServerRequestInterface $request, DelegateInterface $delegate)
     {
-        $accept = $request->getHeader('Accept')[0];
-        if (!$accept || ! preg_match('#^application/([^+\s]+\+)?json#', $accept)) {
-            $request = $request->withAttribute("OUTPUT_TYPE", "html");
-        } else {
-          $request = $request->withAttribute("OUTPUT_TYPE", "json");
-        }
-
-        return $delegate->process($request);
+      $accept = $request->getHeader('Accept')[0];
+      if (!$accept || ! preg_match('#^application/([^+\s]+\+)?json#', $accept)) {
+          $request = $request->withAttribute("OUTPUT_TYPE", "html");
+      } else {
+        $request = $request->withAttribute("OUTPUT_TYPE", "json");
+      }
+      return $delegate->process($request);
     }
 }
-?>
-
