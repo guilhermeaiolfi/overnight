ce9016d1477e7a948927fc67feb12dd6caaec0b6
Guilherme Aiolfi <gradinf@gmail.com>
2017-10-23 10:15:15 -0200

    So you can use unset($user["something"]) and $user["something"]

--- src/User/StandardUser.php
+++ src/User/StandardUser.php
@@ -8,6 +8,15 @@ class StandardUser implements UserInterface {
     $this->data = isset($data)? $data : [];
   }
 
+  public function __unset($name)
+  {
+    unset($this->data[$name]);
+  }
+
+  public function __set ($name, $value) {
+    $this->data[$name] = $value;
+  }
+
   public function __get ($name) {
     if (isset($this->data[$name])) {
         return $this->data[$name];
